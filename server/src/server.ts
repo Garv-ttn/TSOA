708040379b6aff3ddf50f6c753e06eeaaef52e66
garvthakur123 <garv.tomar@tothenew.com>
2022-08-30 23:25:34 +0530

    Served front-end from TSOA server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,15 @@
+const express = require('express');
 import { app } from "./app";
 import dbconnect from "./db/dbconnect";
+
+const path = require('path');
+
+app.use(express.static(path.join(__dirname,'build1')));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'build1','index.html'))
+});
+
 
 const port = process.env.PORT || 7070;
 
